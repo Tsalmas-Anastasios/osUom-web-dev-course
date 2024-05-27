608358ba47d90e048a1dcfa158fe8e43b6158c7c
Tsalmas Anastasios <tsalmanastasios@gmail.com>
2024-05-27 10:16:22 +0300

    lesson 7

---
+++ 2024/1st circle/code/script.js
@@ -0,0 +1,87 @@
+// let comparison = 1 > 2;
+// console.log(comparison);
+
+// // >  bigger than
+// // <  smaller than
+// // == equals
+// // ===  equals
+// // >=   bigger or equal
+// // <= small than or equal
+// // != difference
+// // !== difference
+
+
+// // logical operators
+// // AND --> &&
+// // OR   --> ||
+// // NOT  --> !
+
+
+// // AND
+// let x = 5;
+// let y = 4;
+// let comp1 = x > y;          // --> true
+// let comp2 = x === y;        // --> false
+
+// let comp_total = comp1 && comp2;        // true && false
+
+
+// // OR
+// let x2 = 5;
+// let y2 = 4;
+// let comp1or = x > y;          // --> true
+// let comp2or = x === y;        // --> false
+
+// let comp_total2 = comp1or || comp2or;           // true || false
+
+
+// // NOT
+// let bool_val = x > y;           // true
+// let bool_val2 = !bool_val;      // NOT true --> false
+
+
+
+
+// if condition
+// if (x > y) {
+//     console.log("X is bigger than Y");
+// } else if (x < y) {
+//     console.log("Y is bigger than X");
+// } else {
+//     console.log("X is equal with Y");
+// }
+
+
+// let result = x > y ? 2 : 3;
+
+// if (x > y)
+//     result = 2;
+// else
+//     result = 3;
+
+
+// let result2 = result || 1;
+// result2 = result ?? 1;
+
+
+
+
+// LOOPS
+// for
+// while
+// do...while
+
+
+// for
+// for (let i = 0; i <= 10; i++) {
+//     // do something...
+//     console.log(i);
+// }
+
+
+// while
+// let x = 9;
+// let y = 3;
+// while (x > y) {
+//     break;
+// }
